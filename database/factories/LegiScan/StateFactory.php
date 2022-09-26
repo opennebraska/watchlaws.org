3c92fd37070b65dce4ef6c1b854cb4e6fb56a644
PeterDKC <yinrunning@gmail.com>
2022-09-26 10:07:01 -0500

    Get PHP tests running

---
+++ database/factories/LegiScan/StateFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\LegiScan;
+
+use Illuminate\Support\Arr;
+use App\Models\LegiScan\State;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class StateFactory extends Factory
+{
+    public $model = State::class;
+
+    public function definition()
+    {
+        return [
+            'state_abbr' => $this->faker->stateAbbr,
+            'state_name' => $this->faker->state,
+            'biennium' => 0,
+            'carry_over' => Arr::random(['YES', 'NO']),
+            'capitol' => $this->faker->city,
+            'latitude' => $this->faker->randomFloat(),
+            'longitude' => $this->faker->randomFloat(),
+        ];
+    }
+}
---
+++ tests/Unit/Models/LegiScan/StateTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit\Models\LegiScan;
+
+use Tests\TestCase;
+use App\Models\LegiScan\Bill;
+use App\Models\LegiScan\State;
+
+class StateTest extends TestCase
+{
+    /** @test */
+    public function statesHaveManyBills()
+    {
+        $state = State::factory()->create();
+        Bill::factory()->create([
+            'state_id'    => $state->state_id,
+            'description' => 'A saucy little piece of legislation.',
+        ]);
+
+        $this->assertInstanceOf(
+            Bill::class,
+            $state->bills->first()
+        );
+
+        $this->assertEquals(
+            'A saucy little piece of legislation.',
+            $state->bills->first()->description
+        );
+    }
+}
