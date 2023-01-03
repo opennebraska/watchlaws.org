ce4dc17927a72b22fbad5e898717aa27fdaae8a7
Biagio Arobba <barobba@gmail.com>
2023-01-03 09:59:03 -0600

    Added login validation messages

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -5,6 +5,16 @@
 
         <h1 class="font-bold text-gray-500 pb-3">Login</h1>
 
+        @if ($errors->any())
+            <div class="mb-4 px-3 py-3 w-80 bg-pink-200 text-red-800 text-sm">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li class="ml-4 mb-2 last:mb-0 list-disc">{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         <form action="{{ route('login') }}" method="POST">
             @csrf
             <div class="pb-2">
