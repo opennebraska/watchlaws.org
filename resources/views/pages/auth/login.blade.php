f41785ea013aeedde61cc32cb572aa79e56eb1d9
Biagio Arobba <barobba@gmail.com>
2023-01-03 09:47:14 -0600

    Added border to login fields

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -3,14 +3,14 @@
 @push('body')
     <x-container>
 
-        <h1 class="font-bold text-gray-400 pb-3">Login</h1>
+        <h1 class="font-bold text-gray-500 pb-3">Login</h1>
 
         <form action="{{ route('login') }}" method="POST">
             @csrf
             <div class="pb-2">
 
                 <input type="text" name="email"
-                    class="px-2 py-2 w-80"
+                    class="px-2 py-2 w-80 border border-slate-300"
                     placeholder="Your email address"
                     />
 
@@ -18,7 +18,7 @@
             <div class="pb-2">
 
                 <input type="password" name="password"
-                    class="px-2 py-2 w-80"
+                    class="px-2 py-2 w-80 border border-slate-300"
                     placeholder="Password for this site"
                     />
 
