568374a2b26cc0637ab29097caa531e9b5caf33d
Biagio Arobba <barobba@gmail.com>
2023-01-08 13:11:06 -0600

    Removed functions that only threw exceptions

--- app/Models/UpDownVote.php
+++ app/Models/UpDownVote.php
@@ -19,49 +19,4 @@ class UpDownVote extends Model
     ];
 
     #endregion
-
-    #region Static methods
-
-    // UpDownVote::tally($bill->id, $group->id);
-    // UpDownVote::votes($bill_id, $group->id);
-    // UpDownVote::userVote($bill_id);
-    // UpDownVote::userVoteUp($bill_id);
-    // UpDownVote::userVoteDown($bill_id);
-    // UpDownVote::userVoteClear($bill_id);
-
-    public static function tally($bill_id, $group = null)
-    {
-        // A. Tally for all users
-        // B. Tally for all users who belong to group and it's descendant groups
-        throw new Exception('Not implemented');
-    }
-
-    public static function votes($bill_id, $group = null)
-    {
-        // A. Get for all users
-        // B. Get for all users who belong to group and it's descendant groups
-        throw new Exception('Not implemented');
-    }
-
-    public static function userVote($bill_id, $user_id = null)
-    {
-        throw new Exception('Not implemented');
-    }
-
-    public static function userVoteUp($bill_id, $user_id = null)
-    {
-        throw new Exception('Not implemented');
-    }
-
-    public static function voteDown($bill_id, $user_id = null)
-    {
-        throw new Exception('Not implemented');
-    }
-
-    public static function voteClear($bill_id, $user_id = null)
-    {
-        throw new Exception('Not implemented');
-    }
-
-    #endregion
 }
