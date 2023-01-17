71a9c667033367a3c6aae325035a5777c29edb52
Biagio Arobba <barobba@gmail.com>
2023-01-16 22:28:22 -0600

    Removed commented out code

--- resources/views/pages/user/group/partials/saved-navigation-choices.blade.php
+++ resources/views/pages/user/group/partials/saved-navigation-choices.blade.php
@@ -2,18 +2,12 @@
 
     <div class="bg-gray-200 px-4 py-4">
 
-        {{-- <span class="text-xl">
-            {{ $group->chosenYear() }}
-        </span>
-        <a href="" class="underline">change</a> --}}
-
         {{-- Years --}}
         <form action="{{ route('group.navigate.year.update', $group) }}" method="POST">
             @csrf
             @method('PUT')
             <select name="year" id="year" onchange="this.form.submit()">
                 @foreach ($session_years as $year)
-                    {{-- Check the current --}}
                     <option value="{{ $year }}" {!! $year == $group->chosenYear() ? 'selected="selected"' : '' !!}>
                         {{ $year }}
                     </option>
@@ -24,25 +18,16 @@
     </div>
     <div class="bg-gray-200 px-4 py-4 flex-grow">
 
-        {{-- @if ($group->chosenState())
-            <span class="text-xl">
-                {{  config('enum.legiscan_states')[$group->chosenState()] }}
-            </span>
-        @endif
-        <a href="" class="underline">{{ $group->chosenState() ? 'change' : 'pick state' }}</a> --}}
-
+        {{-- States --}}
         <form action="{{ route('group.navigate.state.update', $group) }}" method="POST">
             @csrf
             @method('PUT')
             <select name="state" id="state" onchange="this.form.submit()">
                 <option value="">ALL STATES</option>
                 @foreach (config('enum.legiscan_states') as $state_abbr => $state_name)
-
-                    {{-- Check the current --}}
                     <option value="{{ $state_abbr }}" {!! $state_abbr == ($group->chosenState()->abbreviation ?? null) ? 'selected="selected"' : '' !!}>
                         {{ $state_name }}
                     </option>
-
                 @endforeach
             </select>
         </form>
