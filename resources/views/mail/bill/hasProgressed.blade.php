adac569b36c2903b3f7e81cf08dba4ca30349c0b
Biagio Arobba <barobba@gmail.com>
2023-01-26 17:31:32 -0600

    Added more description to email, and a link to the state website

--- resources/views/mail/bill/hasProgressed.blade.php
+++ resources/views/mail/bill/hasProgressed.blade.php
@@ -1,21 +1,42 @@
-@component('mail::message')
-# PROGRESS NOTIFICATION
+@php
 
-**{{ $history->bill->number }}** - {{ $history->bill->title }}
+    $bill = $history->bill;
+
+@endphp
+@component('mail::message')
+# BILL NOTIFICATION
+
+**{{ $bill->number }}** &ndash; {{ $bill->title }}
 
 @component('mail::panel')
-{{ $history->action }}
-@if ($history->nebraska_hearing_date_humanized)
+**{{ $history->action }}**<br />
+{{ $history->nebraska_hearing_date_humanized ?? '' }}
 
-{{ $history->nebraska_hearing_date_humanized }}
-@endif
+<sup>Logged {{ $history->history_date->format('Y-m-d') }}</sup>
 @endcomponent
 
-You are being notified because you belong to the following groups that have bookmarked this bill:
+Sponsors:<br />
+@foreach ($bill->sponsors ?? [] as $sponsor)
+{{ $sponsor->person?->name }} ({{ $sponsor->person?->party?->name }}) &ndash; {{ $sponsor->type->description }}<br />
+@endforeach
+
+@if ($bill->pendingCommittee)
+Committee:<br />
+{{ $bill->pendingCommittee->name }}
+@endif
+
+[{{ $bill->state_url }}]({{ $bill->state_url }})
+
+@if ($bill->title != $bill->description)
+{{ $bill->description }}
+@endif
+
+<br />
+
+<sup>You are being notified because you belong to the following groups where this bill is bookmarked:</sup>
 
 @foreach ($relatedGroups as $group)
-@component('mail::button', ['url' => route('groups.show', $group)])
-{{ $group->name }}
-@endcomponent
+<sup>{{ $group->name }}</sup>
+
 @endforeach
 @endcomponent
