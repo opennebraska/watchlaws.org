874d64a1bfe2a845d6984dc6cef526abf431ad6f
Biagio Arobba <barobba@gmail.com>
2023-01-29 07:27:08 -0600

    Added "all hearings" to group header

--- resources/views/groups/partials/header.blade.php
+++ resources/views/groups/partials/header.blade.php
@@ -7,5 +7,6 @@
         <a href="{{ route('groups.workspaces.index', $group) }}" class="hover:underline text-gray-500 ml-1">workspaces</a>
         <a href="{{ route('groups.members.index', $group) }}" class="hover:underline text-gray-500 ml-2">members</a>
         <a href="{{ route('groups.bookmarks.index', $group) }}" class="hover:underline text-gray-500 ml-2">all bookmarks</a>
+        <a href="{{ route('groups.hearings.index', $group) }}" class="hover:underline text-gray-500 ml-2">all hearings</a>
     </nav>
 </div>
